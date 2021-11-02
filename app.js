d8a7d8713e4fd7f8f6bce26eae8fe1c0a08df0a8
hasan <hasandemir.5802@gmail.com>
2021-11-02 21:58:39 +0300

    ödev8

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const ejs = require('ejs')
+const path = require('path');
+const app = express();
+//TEMPLATE ENGİNE
+app.set("view engine","ejs")
+
+//MİDDLEWARES
+app.use(express.static('public'));
+//ROUTES
+const port = 3001;
+app.get('/', (req, res) => {
+  res.render('index')
+});
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/add', (req, res) => {
+  res.render('add')
+});
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
